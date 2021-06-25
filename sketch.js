676c920d78f70038733272c70370a34d801d4753
pro-whitehatjr <80043809+pro-whitehatjr@users.noreply.github.com>
2021-06-25 11:17:31 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
 
 var trex ,trex_running;
 function preload(){
-  trex_running = loadAnimation("trex1.png", "trex3.png", "trex4.png");
+  
 
 }
 
@@ -9,12 +9,11 @@ function setup(){
   createCanvas(600,200)
   
   //create a trex sprite
-  trex = createSprite(50,160,20,50);
-  trex.addAnimation("running", trex_running);
+ 
 }
 
 function draw(){
   background("white")
-  drawSprites();
+  
 
 }
